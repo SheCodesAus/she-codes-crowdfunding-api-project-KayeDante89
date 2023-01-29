0c77bc35a9d8ccee980fe78450a97b0765784bba
KayeDante89 <k.flamenco@hotmail.com>
2023-01-29 14:30:26 +0800

    Added Search Filter

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from rest_framework import status, generics, permissions
+from rest_framework import status, generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Project, Pledge
@@ -27,12 +27,18 @@ from .permissions import IsOwnerOrReadOnly, IsSupporterOrReadOnly
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# class DynamicSearchFilter(filters.SearchFilter):
+
+#     def get_search_fields(self, view, request):
+#         return request.GET.getlist('search_fields', [])
+
 class ProjectList(generics.ListCreateAPIView):
 
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter,]
     filterset_fields = ["owner", "is_open"]
+    search_fields = ["title", "description"]
 
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
@@ -100,4 +106,3 @@ class PledgeDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Pledge.objects.all()
     serializer_class = PledgeSerializer
-    
